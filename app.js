dccd50d33f0d3c624246dda685c30942ec253752
hani abdulle <haniamira24@gmail.com>
2022-07-09 09:51:46 -0600

    arrays

---
+++ app.js
@@ -0,0 +1,18 @@
+let itemnames = ["water,juice,pop"];
+let itemPrices = [2,3,2]
+let inStock = [false,true,false]
+
+let firstitemname = itemnames[0];
+console.log(firstitemname);
+
+let firstitemPrice = itemPrices[0];
+console.log(firstitemPrice);
+itemnames.push('coke');
+itemPrices.push(2);
+inStock.push(true);
+lastitemname = itemnames.pop();
+lastitemprice = itemprices.pop();
+lastitemstock = instock.pop();
+
+
+
